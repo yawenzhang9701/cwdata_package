0ad3fdfbc282ea1c60cdc916b69468e7f4d7c29e
Yawen Zhang <yzha0275@student.monash.edu>
2020-10-12 19:01:00 +0800

    add documenttation

---
+++ R/key_crop_yields.R
@@ -0,0 +1,10 @@
+#' Global key crop yields
+#'
+#' This data set contains crop yields from 249 countries.
+#'
+#' TODO: Add description of the columns
+#'
+#' @source
+#' The data below comes from Our World in Data and sourced from Tidy Tuesday.
+
+"key_crop_yields"
